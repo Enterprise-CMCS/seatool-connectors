2f8a0238529748606eccdeddd6c14f89c0ddff2b
Mike Dial <48921055+mdial89f@users.noreply.github.com>
2024-02-26 06:26:16 -0500

    new services tuned for performance

---
+++ faststop.sh
@@ -0,0 +1,2 @@
+
+aws ecs update-service --no-cli-pager --cluster seatool-ksqlom-$1-connect --service ksqldb-headless --desired-count 0
---
+++ src/services/ksqlom/ddl/seatool/manifest.js
@@ -0,0 +1,7 @@
+module.exports.manifest = [
+  "k_sp_streams.sql",
+  "k_sp_tables.sql",
+  "k_sp_child.sql",
+  "k_sp_parent.sql",
+  "k_state_plan.sql",
+];
