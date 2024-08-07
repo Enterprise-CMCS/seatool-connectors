947d19c6f3537c44af3b0be90e87983314f796d9
Mike Dial <michael.dial.4@gmail.com>
2024-08-07 16:47:54 -0400

    update to point to v2 debezim  topics

--- src/services/ksqlomcd/ddl/seatool/k_sp_streams.sql
+++ src/services/ksqlomcd/ddl/seatool/k_sp_streams.sql
@@ -6,7 +6,7 @@ CREATE STREAM IF NOT EXISTS K_seatool_Action_Officers_stream (
       ID_Number varchar,
       Officer_ID integer>>
 )
-WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.SEA.dbo.Action_Officers',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
+WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.v2.SEA.dbo.Action_Officers',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
 
 CREATE STREAM IF NOT EXISTS K_seatool_Action_Types_stream (
   PK STRUCT <payload STRUCT <Action_ID integer>> KEY,
@@ -15,7 +15,7 @@ CREATE STREAM IF NOT EXISTS K_seatool_Action_Types_stream (
     Action_Name varchar,
     Plan_Type_ID integer>>
 )
-WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.SEA.dbo.Action_Types',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
+WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.v2.SEA.dbo.Action_Types',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
 
 CREATE STREAM IF NOT EXISTS K_seatool_aspnet_Users_stream (
   PK STRUCT <payload STRUCT <UserId varchar>> KEY,
@@ -28,7 +28,7 @@ CREATE STREAM IF NOT EXISTS K_seatool_aspnet_Users_stream (
     IsAnonymous boolean,
     LastActivityDate timestamp>>
 )
-WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.SEA.dbo.aspnet_Users',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
+WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.v2.SEA.dbo.aspnet_Users',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
 
 CREATE STREAM IF NOT EXISTS K_seatool_Call_Held_Reasons_stream (
   PK STRUCT <payload STRUCT <Call_Held_Reason_ID integer>> KEY,
@@ -36,7 +36,7 @@ CREATE STREAM IF NOT EXISTS K_seatool_Call_Held_Reasons_stream (
     Call_Held_Reason_ID integer,
     Reason_Description varchar>>
 )
-WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.SEA.dbo.Call_Held_Reasons',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
+WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.v2.SEA.dbo.Call_Held_Reasons',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
 
 CREATE STREAM IF NOT EXISTS K_seatool_Code_After_Init_Assess_stream (
   PK STRUCT <payload STRUCT <Code_after_init_assess_ID integer>> KEY,
@@ -44,7 +44,7 @@ CREATE STREAM IF NOT EXISTS K_seatool_Code_After_Init_Assess_stream (
     Code_after_init_assess_ID integer,
     Code_after_init_assess_Desc varchar>>
 )
-WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.SEA.dbo.Code_After_Init_Assess',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
+WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.v2.SEA.dbo.Code_After_Init_Assess',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
 
 CREATE STREAM IF NOT EXISTS K_seatool_Components_stream (
   PK STRUCT <payload STRUCT <Component_ID integer>> KEY,
@@ -53,7 +53,7 @@ CREATE STREAM IF NOT EXISTS K_seatool_Components_stream (
     Component_Name varchar,
     Alerts_Inbox_Address varchar>>
 )
-WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.SEA.dbo.Components',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
+WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.v2.SEA.dbo.Components',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
 
 CREATE STREAM IF NOT EXISTS K_seatool_Components_StatePlans_stream (
   PK STRUCT <payload STRUCT <ID_Number varchar, Component_ID integer>> KEY,
@@ -61,7 +61,7 @@ CREATE STREAM IF NOT EXISTS K_seatool_Components_StatePlans_stream (
     ID_Number varchar,
     Component_ID integer>>
 )
-WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.SEA.dbo.Components_StatePlans',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
+WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.v2.SEA.dbo.Components_StatePlans',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
 
 CREATE STREAM IF NOT EXISTS K_seatool_Early_Alert_Field_Types_stream (
   PK STRUCT <payload STRUCT <EA_Type_ID integer>> KEY,
@@ -69,7 +69,7 @@ CREATE STREAM IF NOT EXISTS K_seatool_Early_Alert_Field_Types_stream (
     EA_Type_ID integer,
     EA_Type_Description varchar>>
 )
-WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.SEA.dbo.Early_Alert_Field_Types',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
+WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.v2.SEA.dbo.Early_Alert_Field_Types',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
 
 CREATE STREAM IF NOT EXISTS K_seatool_Early_Alert_Fields_stream (
   PK STRUCT <payload STRUCT <EA_Field_ID integer>> KEY,
@@ -84,7 +84,7 @@ CREATE STREAM IF NOT EXISTS K_seatool_Early_Alert_Fields_stream (
     Active boolean,
     Is_APD boolean>>
 )
-WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.SEA.dbo.Early_Alert_Fields',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
+WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.v2.SEA.dbo.Early_Alert_Fields',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
 
 CREATE STREAM IF NOT EXISTS K_seatool_EmailDistribution_stream (
   PK STRUCT <payload STRUCT <EmailDistributionID integer>> KEY,
@@ -93,7 +93,7 @@ CREATE STREAM IF NOT EXISTS K_seatool_EmailDistribution_stream (
     EmailID integer,
     EmailAddress varchar>>
 )
-WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.SEA.dbo.EmailDistribution',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
+WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.v2.SEA.dbo.EmailDistribution',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
 
 CREATE STREAM IF NOT EXISTS K_seatool_Email_stream (
   PK STRUCT <payload STRUCT <EmailID integer>> KEY,
@@ -103,7 +103,7 @@ CREATE STREAM IF NOT EXISTS K_seatool_Email_stream (
     FileName varchar,
     DisplayInAdmin boolean>>
 )
-WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.SEA.dbo.Email',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
+WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.v2.SEA.dbo.Email',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
 
 CREATE STREAM IF NOT EXISTS K_seatool_OCD_Review_stream (
   PK STRUCT <payload STRUCT <OCD_Review_ID integer>> KEY,
@@ -111,7 +111,7 @@ CREATE STREAM IF NOT EXISTS K_seatool_OCD_Review_stream (
     OCD_Review_ID integer,
     OCD_Review_Description varchar>>
 )
-WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.SEA.dbo.OCD_Review',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
+WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.v2.SEA.dbo.OCD_Review',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
 
 CREATE STREAM IF NOT EXISTS K_seatool_Officers_stream (
   PK STRUCT <payload STRUCT <Officer_ID integer>> KEY,
@@ -124,7 +124,7 @@ CREATE STREAM IF NOT EXISTS K_seatool_Officers_stream (
     Position_ID integer,
     Email varchar>>
 )
-WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.SEA.dbo.Officers',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
+WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.v2.SEA.dbo.Officers',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
 
 CREATE STREAM IF NOT EXISTS K_seatool_SPA_Type_stream (
   PK STRUCT <payload STRUCT <SPA_Type_ID integer>> KEY,
@@ -133,7 +133,7 @@ CREATE STREAM IF NOT EXISTS K_seatool_SPA_Type_stream (
     SPA_Type_Name varchar,
     Plan_Type_ID integer>>
 )
-WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.SEA.dbo.SPA_Type',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
+WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.v2.SEA.dbo.SPA_Type',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
 
 CREATE STREAM IF NOT EXISTS K_seatool_Position_stream (
   PK STRUCT <payload STRUCT <Position_ID integer>> KEY,
@@ -141,7 +141,7 @@ CREATE STREAM IF NOT EXISTS K_seatool_Position_stream (
     Position_ID integer,
     Position_Name varchar>>
 )
-WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.SEA.dbo.Position',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
+WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.v2.SEA.dbo.Position',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
 
 CREATE STREAM IF NOT EXISTS K_seatool_Priority_Codes_stream (
   PK STRUCT <payload STRUCT <Priority_Code_ID integer>> KEY,
@@ -150,7 +150,7 @@ CREATE STREAM IF NOT EXISTS K_seatool_Priority_Codes_stream (
     Priority_Code varchar,
     Priority_Code_Description varchar>>
 )
-WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.SEA.dbo.Priority_Codes',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
+WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.v2.SEA.dbo.Priority_Codes',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
 
 CREATE STREAM IF NOT EXISTS K_seatool_Priority_Complexity_stream (
   PK STRUCT <payload STRUCT <Priority_Complexity_ID integer>> KEY,
@@ -159,7 +159,7 @@ CREATE STREAM IF NOT EXISTS K_seatool_Priority_Complexity_stream (
     Priority_Complexity_Value varchar,
     Priority_Complexity_Description varchar>>
 )
-WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.SEA.dbo.Priority_Complexity',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
+WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.v2.SEA.dbo.Priority_Complexity',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
 
 CREATE STREAM IF NOT EXISTS K_seatool_Priority_Review_Position_stream (
   PK STRUCT <payload STRUCT <Review_Position_ID integer>> KEY,
@@ -168,7 +168,7 @@ CREATE STREAM IF NOT EXISTS K_seatool_Priority_Review_Position_stream (
     Review_Position_Code varchar,
     Review_Position_Description varchar>>
 )
-WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.SEA.dbo.Priority_Review_Position',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
+WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.v2.SEA.dbo.Priority_Review_Position',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
 
 CREATE STREAM IF NOT EXISTS K_seatool_RAI_stream (
   PK STRUCT <payload STRUCT <ID_Number varchar, RAI_Requested_Date timestamp>> KEY,
@@ -179,7 +179,7 @@ CREATE STREAM IF NOT EXISTS K_seatool_RAI_stream (
       RAI_Received_Date timestamp,
       RAI_Withdrawn_Date timestamp>>
 )
-WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.SEA.dbo.RAI',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
+WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.v2.SEA.dbo.RAI',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
 
 CREATE STREAM IF NOT EXISTS K_seatool_Region_stream (
   PK STRUCT <payload STRUCT <Region_ID varchar>> KEY,
@@ -188,7 +188,7 @@ CREATE STREAM IF NOT EXISTS K_seatool_Region_stream (
     Region_Name varchar,
     Alerts_Inbox_Address varchar>>
 )
-WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.SEA.dbo.Region',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
+WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.v2.SEA.dbo.Region',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
 
 CREATE STREAM IF NOT EXISTS K_seatool_Region_Access_stream (
   PK STRUCT <payload STRUCT <UserId varchar, Region_ID varchar>> KEY,
@@ -197,7 +197,7 @@ CREATE STREAM IF NOT EXISTS K_seatool_Region_Access_stream (
     Region_ID varchar,
     Type_Of_Access varchar>>
 )
-WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.SEA.dbo.Region_Access',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
+WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.v2.SEA.dbo.Region_Access',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
 
 CREATE STREAM IF NOT EXISTS K_seatool_State_Plan_Service_Types_stream (
   PK STRUCT <payload STRUCT <ID_Number varchar, Service_Type_ID integer>> KEY,
@@ -205,7 +205,7 @@ CREATE STREAM IF NOT EXISTS K_seatool_State_Plan_Service_Types_stream (
     ID_Number varchar,
     Service_Type_ID integer>>
 )
-WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.SEA.dbo.State_Plan_Service_Types',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
+WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.v2.SEA.dbo.State_Plan_Service_Types',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
 
 CREATE STREAM IF NOT EXISTS K_seatool_State_Plan_1115_stream (
   PK STRUCT <payload STRUCT <ID_Number varchar>> KEY,
@@ -225,7 +225,7 @@ CREATE STREAM IF NOT EXISTS K_seatool_State_Plan_1115_stream (
     FRT_Response_Deadline timestamp,
     Disapproval_Track boolean>>
 )
-WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.SEA.dbo.State_Plan_1115',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
+WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.v2.SEA.dbo.State_Plan_1115',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
 
 CREATE STREAM IF NOT EXISTS K_seatool_State_Plan_APD_stream (
   PK STRUCT <payload STRUCT <ID_Number varchar>> KEY,
@@ -235,7 +235,7 @@ CREATE STREAM IF NOT EXISTS K_seatool_State_Plan_APD_stream (
     Sharepoint_Link varchar,
     Email_Sent boolean>>
 )
-WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.SEA.dbo.State_Plan_APD',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
+WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.v2.SEA.dbo.State_Plan_APD',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
 
 CREATE STREAM IF NOT EXISTS K_seatool_State_Plan_APD_Sub_Type_stream (
   PK STRUCT <payload STRUCT <ID_Number varchar, Sub_Type_ID integer>> KEY,
@@ -243,7 +243,7 @@ CREATE STREAM IF NOT EXISTS K_seatool_State_Plan_APD_Sub_Type_stream (
     ID_Number varchar,
     Sub_Type_ID integer>>
 )
-WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.SEA.dbo.State_Plan_APD_Sub_Type',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
+WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.v2.SEA.dbo.State_Plan_APD_Sub_Type',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
 
 CREATE STREAM IF NOT EXISTS K_seatool_State_Plan_Early_Alerts_stream (
   PK STRUCT <payload STRUCT <ID_Number varchar, EA_Field_ID integer>> KEY,
@@ -254,7 +254,7 @@ CREATE STREAM IF NOT EXISTS K_seatool_State_Plan_Early_Alerts_stream (
     Text_DD_Val varchar,
     Email_Sent boolean>>
 )
-WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.SEA.dbo.State_Plan_Early_Alerts',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
+WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.v2.SEA.dbo.State_Plan_Early_Alerts',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
 
 CREATE STREAM IF NOT EXISTS K_seatool_State_Plan_Impact_Funding_stream (
   PK STRUCT <payload STRUCT <ID_Number varchar>> KEY,
@@ -272,7 +272,7 @@ CREATE STREAM IF NOT EXISTS K_seatool_State_Plan_Impact_Funding_stream (
     Provider_Tax boolean,
     Other boolean>>
 )
-WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.SEA.dbo.State_Plan_Impact_Funding',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
+WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.v2.SEA.dbo.State_Plan_Impact_Funding',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
 
 CREATE STREAM IF NOT EXISTS K_seatool_States_stream (
   PK STRUCT <payload STRUCT <State_Code varchar>> KEY,
@@ -282,7 +282,7 @@ CREATE STREAM IF NOT EXISTS K_seatool_States_stream (
     State_Name varchar,
     Priority_Flag boolean>>
 )
-WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.SEA.dbo.States',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
+WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.v2.SEA.dbo.States',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
 
 CREATE STREAM IF NOT EXISTS K_seatool_Stop_Resume_Dates_stream (
   PK STRUCT <payload STRUCT <ID_Number varchar, Stop_Date timestamp>> KEY,
@@ -291,7 +291,7 @@ CREATE STREAM IF NOT EXISTS K_seatool_Stop_Resume_Dates_stream (
     Stop_Date timestamp,
     Resume_Date timestamp>>
 )
-WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.SEA.dbo.Stop_Resume_Dates',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
+WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.v2.SEA.dbo.Stop_Resume_Dates',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
 
 CREATE STREAM IF NOT EXISTS K_seatool_Sub_Type_stream (
   PK STRUCT <payload STRUCT <Sub_Type_ID integer>> KEY,
@@ -300,7 +300,7 @@ CREATE STREAM IF NOT EXISTS K_seatool_Sub_Type_stream (
     Sub_Type_Name varchar,
     Type_ID integer>>
 )
-WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.SEA.dbo.Sub_Type',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
+WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.v2.SEA.dbo.Sub_Type',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
 
 CREATE STREAM IF NOT EXISTS K_seatool_Title_Type_stream (
   PK STRUCT <payload STRUCT <Title_Type_ID integer>> KEY,
@@ -308,7 +308,7 @@ CREATE STREAM IF NOT EXISTS K_seatool_Title_Type_stream (
     Title_Type_ID integer,
     Title_Type_Description varchar>>
 )
-WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.SEA.dbo.Title_Type',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
+WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.v2.SEA.dbo.Title_Type',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
 
 CREATE STREAM IF NOT EXISTS K_seatool_Type_stream (
   PK STRUCT <payload STRUCT <Type_Id integer>> KEY,
@@ -318,7 +318,7 @@ CREATE STREAM IF NOT EXISTS K_seatool_Type_stream (
     Type_Class integer,
     Plan_Type_ID integer>>
 )
-WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.SEA.dbo.Type',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
+WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.v2.SEA.dbo.Type',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
 
 CREATE STREAM IF NOT EXISTS K_seatool_State_Plan_Service_SubTypes_stream (
   PK STRUCT <payload STRUCT <ID_Number varchar, Service_SubType_ID integer>> KEY,
@@ -326,7 +326,7 @@ CREATE STREAM IF NOT EXISTS K_seatool_State_Plan_Service_SubTypes_stream (
     ID_Number varchar,
     Service_SubType_ID integer>>
 )
-WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.SEA.dbo.State_Plan_Service_SubTypes',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
+WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.v2.SEA.dbo.State_Plan_Service_SubTypes',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
 
 CREATE STREAM IF NOT EXISTS K_seatool_State_Plan_stream (
   PK STRUCT <payload STRUCT <ID_Number varchar>> KEY,
@@ -412,5 +412,5 @@ CREATE STREAM IF NOT EXISTS K_seatool_State_Plan_stream (
     Added_Cost boolean,
     Organization_Change boolean>>
 )
-WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.SEA.dbo.State_Plan',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
+WITH (KAFKA_TOPIC='${param:topicNamespace}aws.seatool.debezium.cdc.v2.SEA.dbo.State_Plan',VALUE_FORMAT='JSON',KEY_FORMAT='JSON');
 
