28844e57ce07e98bb57fd14c6ed3a77562960bb3
Benjamin Paige <benjamin.paige@gmail.com>
2025-07-27 22:28:15 -0600

    chore: enhance testing configuration and CDK context
    - Updated the coverage script in package.json to include a specific config file for Vitest.
    - Enhanced the Vitest configuration to specify coverage options, including report formats and exclusions.
    - Moved the bootstrap qualifier for CDK into the context section for better organization and clarity.

--- src/tests/vitest.config.ts
+++ src/tests/vitest.config.ts
@@ -1,5 +1,19 @@
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  test: {},
+  test: {
+    coverage: {
+      provider: 'c8',
+      reporter: ['text', 'clover', 'json', 'html'],
+      reportsDirectory: './coverage',
+      include: ['src/**/*.{js,ts}'],
+      exclude: [
+        'src/tests/**',
+        'src/**/*.test.{js,ts}',
+        'src/**/*.spec.{js,ts}',
+        'cdk/**',
+        'node_modules/**'
+      ]
+    }
+  },
 });
